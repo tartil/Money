fbf6fbaabde1e7d5674dfe7054dbdc138bf3d3ce
tartil <kovalchuk.oksana17@gmail.com>
2017-05-03 12:29:18 +0300

    adding main folder

---
+++ Purse/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Purse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Money m = new Money(123);
+            m.GetInformation();
+        }
+    }
+}
